e46453523269c750df10482d84a091cff6b25cff
Michał <michal.reszka86@gmail.com>
2024-04-25 21:04:07 +0200

    Add JWT initial config, service and dependencies

---
+++ src/main/java/pl/pollub/integracja_projekt/Config/SecurityConfiguration.java
@@ -0,0 +1,33 @@
+package pl.pollub.integracja_projekt.Config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import pl.pollub.integracja_projekt.Services.JwtService;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class SecurityConfiguration extends OncePerRequestFilter {
+
+    private JwtService jwtService;
+    
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request,@NonNull HttpServletResponse response,@NonNull FilterChain filterChain) throws ServletException, IOException {
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        final String userEmail;
+        if(authHeader == null || !authHeader.startsWith("Bearer ")){
+            filterChain.doFilter(request, response);
+            return;
+        }
+        jwt = authHeader.substring(7);
+        userEmail = jwtService.extractUsername(jwt);
+    }
+}
---
+++ src/main/java/pl/pollub/integracja_projekt/Services/JwtService.java
@@ -0,0 +1,40 @@
+package pl.pollub.integracja_projekt.Services;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Base64;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+
+    private static final String SECRET_KEY = "c2f47d55cd6f8ff1877f98786243b81eb961612293bb9aa3df90d4e19757eb19";
+
+    public String extractUsername(String token){
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver){
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token){
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignInKey(){
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
