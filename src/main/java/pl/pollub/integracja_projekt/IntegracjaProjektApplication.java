d9b0b88205974870cd12a156cba78829be2c844f
Michal Reszka <michal.reszka86@gmail.com>
2024-04-25 18:08:39 +0200

    Initial commit

---
+++ src/main/java/pl/pollub/integracja_projekt/IntegracjaProjektApplication.java
@@ -0,0 +1,13 @@
+package pl.pollub.integracja_projekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegracjaProjektApplication {
+	public static void main(String[] args) {
+
+		SpringApplication.run(IntegracjaProjektApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/pollub/integracja_projekt/IntegracjaProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pollub.integracja_projekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegracjaProjektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
