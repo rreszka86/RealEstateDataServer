c8f47faa1848c682f933a073a23d6f45d061f5eb
sawyyy <kamilsadowski001@gmail.com>
2024-04-26 19:38:11 +0200

    add application configuration

---
+++ src/main/java/pl/pollub/integracja_projekt/Config/ApplicationConfiguration.java
@@ -0,0 +1,21 @@
+package pl.pollub.integracja_projekt.Config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import pl.pollub.integracja_projekt.Repositories.UserRepository;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfiguration {
+    private final UserRepository repository;
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return username -> repository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
