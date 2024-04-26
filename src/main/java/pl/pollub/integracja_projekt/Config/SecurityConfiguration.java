c1f1c0cc583a065147aac85e9ee6bcde6f47ac3f
sawyyy <kamilsadowski001@gmail.com>
2024-04-26 19:38:45 +0200

    finished security configuration

--- src/main/java/pl/pollub/integracja_projekt/Config/SecurityConfiguration.java
+++ src/main/java/pl/pollub/integracja_projekt/Config/SecurityConfiguration.java
@@ -6,6 +6,11 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import pl.pollub.integracja_projekt.Services.JwtService;
@@ -16,7 +21,9 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class SecurityConfiguration extends OncePerRequestFilter {
 
-    private JwtService jwtService;
+    private final JwtService jwtService;
+
+    private final UserDetailsService userDetailsService;
     
     @Override
     protected void doFilterInternal(@NonNull HttpServletRequest request,@NonNull HttpServletResponse response,@NonNull FilterChain filterChain) throws ServletException, IOException {
@@ -29,5 +36,16 @@ public class SecurityConfiguration extends OncePerRequestFilter {
         }
         jwt = authHeader.substring(7);
         userEmail = jwtService.extractUsername(jwt);
+
+        if(userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null){
+            UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
+            if(jwtService.isTokenValid(jwt, userDetails)){
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+        }
+
+        filterChain.doFilter(request, response);
     }
 }
