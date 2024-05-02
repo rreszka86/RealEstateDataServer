859f4c498c1fc3f007eb8443c99854e75f95677d
Michal <michal.reszka86@gmail.com>
2024-05-02 18:36:07 +0200

    Changes by Kamil

---
+++ src/main/java/pl/pollub/integracja_projekt/Config/SecurityConfiguration.java
@@ -0,0 +1,38 @@
+package pl.pollub.integracja_projekt.Config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+
+    private final JwtAuth jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(
+                        (auth) -> auth
+                                .requestMatchers("/api/auth/register", "/api/auth/authenticate").permitAll()
+                                .anyRequest().authenticated()
+
+        )
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
+
+        return http.build();
+    }
+}
---
+++ src/main/java/pl/pollub/integracja_projekt/Controllers/AuthenticationController.java
@@ -0,0 +1,32 @@
+package pl.pollub.integracja_projekt.Controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.pollub.integracja_projekt.Config.AuthenticationRequest;
+import pl.pollub.integracja_projekt.Config.AuthenticationResponse;
+import pl.pollub.integracja_projekt.Config.RegisterRequest;
+import pl.pollub.integracja_projekt.Services.AuthenticationService;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+    private final AuthenticationService service;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request) {
+        System.out.println("czemutoniedziala");
+        return ResponseEntity.ok(service.register(request));
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request){
+        return ResponseEntity.ok(service.authenticate(request));
+    }
+}
