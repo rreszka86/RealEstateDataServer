b44556a79e12ae9993b3808dcbd8729e82965954
Michał <michal.reszka86@gmail.com>
2024-06-04 21:25:55 +0200

    Secure email availability (on register) from dirty reads

--- src/main/java/pl/pollub/integracja_projekt/Repositories/UserRepository.java
+++ src/main/java/pl/pollub/integracja_projekt/Repositories/UserRepository.java
@@ -2,6 +2,7 @@ package pl.pollub.integracja_projekt.Repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 import pl.pollub.integracja_projekt.Models.User;
 
@@ -9,6 +10,6 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
-    @Transactional(readOnly = true)
+    @Transactional(readOnly = true, isolation = Isolation.READ_COMMITTED)
     Optional<User> findByEmail(String email);
 }
